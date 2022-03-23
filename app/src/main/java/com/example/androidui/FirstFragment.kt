013f3704648ec4a11a0260674889b0ae126717d0
Alexander <alez.grante@gmail.com>
2022-03-23 22:36:14 +0300

    popupmenu handler

--- app/src/main/java/com/example/androidui/FirstFragment.kt
+++ app/src/main/java/com/example/androidui/FirstFragment.kt
@@ -1,11 +1,13 @@
 package com.example.androidui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.androidui.databinding.FragmentFirstBinding
 
@@ -30,6 +32,22 @@ class FirstFragment : Fragment() {
         binding.popupBtn.setOnClickListener {
             val popupMenu = PopupMenu(activity,it)
             popupMenu.inflate(R.menu.popupmenu)
+            popupMenu.setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.menu1 -> {
+                        Toast.makeText(activity, "Вы выбрали PopupMenu 1", Toast.LENGTH_LONG).show()
+                        return@setOnMenuItemClickListener true
+                    }
+                    R.id.menu2 -> {
+                        Toast.makeText(activity, "Вы выбрали PopupMenu 2", Toast.LENGTH_LONG).show()
+                        return@setOnMenuItemClickListener true
+                    }
+                    else -> {
+                        Toast.makeText(activity, "Default", Toast.LENGTH_LONG).show()
+                        return@setOnMenuItemClickListener false
+                    }
+                }
+            }
             popupMenu.show()
         }
 
