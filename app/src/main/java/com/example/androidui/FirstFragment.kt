f345e62b7098cc0be7359217f921911fb935e536
Alexander <alez.grante@gmail.com>
2022-03-23 22:26:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidui/FirstFragment.kt
@@ -0,0 +1,52 @@
+package com.example.androidui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.PopupMenu
+import androidx.navigation.fragment.findNavController
+import com.example.androidui.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        binding.popupBtn.setOnClickListener {
+            val popupMenu = PopupMenu(activity,it)
+            popupMenu.inflate(R.menu.popupmenu)
+            popupMenu.show()
+        }
+
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
