c2035a9a18a6fa563caaf67f0be345287cdb9f83
Alexander <alez.grante@gmail.com>
2022-03-23 23:56:21 +0300

    espresso test with exception

---
+++ app/src/androidTest/java/com/example/androidui/PopUpMenuTest.kt
@@ -0,0 +1,37 @@
+/*
+ * *
+ *  * Created by Alexander on 3/23/22, 11:17 PM
+ *  * Copyright (c) 2022 . All rights reserved.
+ *  * Last modified 3/23/22, 11:17 PM
+ *
+ */
+
+package com.example.androidui
+
+import android.support.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class PopUpMenuTest {
+
+    @Rule
+    val popUpMenuActivityTestRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    @Test
+    fun clickPopUpMenuButton_openMenu() {
+        onView(withId(R.id.popup_btn))
+            .perform(click())
+
+        onView(withId(R.id.menugroup1))
+            .check(matches(isDisplayed()))
+
+    }
+}
